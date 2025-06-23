cec0757d82f6c8e321495164b7da53661457c1bc
Divyansh sharma <91496262+divyan154@users.noreply.github.com>
2025-06-23 12:51:30 +0530

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -20,8 +20,7 @@ router
     validateCampground,
     catchAsync(campController.createCampground)
   );
-// keys = ["pexels-quang-nguyen-vinh-222549-2131614.jpg"];
-// const myBucket = process.env.BUCKET_NAME;
+
 
 router.get("/new", isLoggedIn, campController.renderNewForm);
 
